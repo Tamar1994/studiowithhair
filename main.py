24a2ec332fdce98377a105d7e59a4dfdc6f6d2da
Adriano Tamar Rodrigues <adriano.tamar@gmail.com>
2023-01-14 13:59:14 -0300

    atuallizacao

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def homePage():
     return render_template("home.html")
 
+@app.route("/gestao", methods=['GET', 'POST'])
+def gestaoPage():
+    return render_template("gestao.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
